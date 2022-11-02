3ffd4a99cc4c108b20f7b8ce8d1d50ea74c6256a
System Administrator <root@MacBook-Pro-de-El.local>
2022-11-02 23:03:11 +0100

    refactor: add Dockerfile for backend

--- todo-front/src/app/app.component.ts
+++ todo-front/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'emb-the-ghost';
+  title = 'the best in the world TGBL';
 }
