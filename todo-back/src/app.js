6e670d24a22ad99a1d9ad9a85239037c0f06dab6
System Administrator <root@MacBook-Pro-de-El.local>
2022-11-08 22:30:34 +0100

    refact: multi-platform

--- todo-back/src/app.js
+++ todo-back/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello El Mahfoud, i hope ci works!')
+  res.send('Hey El Mahfoud, i hope ci works!')
 })
 
 app.listen(5000)
