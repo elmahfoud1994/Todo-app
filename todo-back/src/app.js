f1a3ffd258440e3a10dfeb12873258a0ca2fecc0
System Administrator <root@MacBook-Pro-de-El.local>
2022-11-08 22:52:10 +0100

    fix: support errors

--- todo-back/src/app.js
+++ todo-back/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hey El Mahfoud, i hope ci works!')
+  res.send('Hey El Mahfoud Bouatim, i hope ci works!')
 })
 
 app.listen(5000)
